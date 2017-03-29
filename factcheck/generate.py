263fcda34af538792f519ca209e42744cf90b09a
Eric Liu <ericliu314@gmail.com>
2017-03-28 23:31:45 -0400

    Fixed generate.py

--- factcheck/generate.py
+++ factcheck/generate.py
@@ -1,43 +1,56 @@
 """
-    test.py
+    generate.py
+
+    From the normalized text files true.txt and false.txt,
+    this module builds up the feature set by taking the
+    most frequent 10,000 words in the true and false datasets
+    filtered by only considering adjectives, adverbs,
+    verbs, nouns, conjunctions, numerals and determinants,
+    and then trains the five classifiers used: Multinomial
+    Naive Bayes, Bernoulli Naive Bayes, Logistic Regression,
+    Stochastic Gradient Descent and Linear Support Vector
+    Machine.
 """
 
+
 import nltk
+import pickle
 import random
 from nltk import word_tokenize
 from nltk.classify.scikitlearn import SklearnClassifier
-import pickle
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.svm import LinearSVC
 
 
-DEBUG = True
-
-
-# Open text dumps
-trueText = open('true.txt', 'r')
-falseText = open('false.txt', 'r')
-
-# Extract out sentence tokens from each text dump
+# Open the normalized true and false datasets
+# and append each line to the trueSentences and
+# falseSentences lists.
 trueSentences = []
 falseSentences = []
 
-for l in trueText:
-    trueSentences.append(l)
+try:
+    trueText = open('textNormalized/true.txt', 'r')
+    falseText = open('textNormalized/false.txt', 'r')
 
-for l in falseText:
-    falseSentences.append(l)
+    for line in trueText:
+        trueSentences.append(line)
 
-if DEBUG:
-    print(trueSentences)
-    print(falseSentences)
+    for line in falseText:
+        falseSentences.append(line)
+except Exception as e:
+    print('[!] Error occurred when reading from text files.')
 
 
-# Create documents and word features, save to pickle
+# Label each sentence in each of trueSentences and
+# falseSentences as True or False respectively
+# in a tuple, then append to documents. Tokenize
+# words in each sentence and only add unique, allowed
+# word types into wordFeatures.
 documents = []
 wordFeatures = []
-allowedWordTypes = ['ADJ', 'ADV', 'VERB', 'N', 'CONJ', 'NUM', 'DET']
+allowedWordTypes = ['ADJ', 'ADV', 'VERB', 'N',
+                    'CONJ', 'NUM', 'DET']
 
 for s in trueSentences:
     documents.append((s, True))
@@ -61,9 +74,6 @@ for s in falseSentences:
 wordFeatures = nltk.FreqDist(wordFeatures)
 wordFeatures = list(wordFeatures.keys())[:10000]
 
-if DEBUG:
-    print(wordFeatures)
-
 savedDocuments = open('saved_documents.pickle', 'wb')
 pickle.dump(documents, savedDocuments)
 savedDocuments.close()
@@ -83,8 +93,11 @@ def findFeatures(document):
     return features
 
 
+# Feature sets are built up by iterating through
+# each element in documents and checking if the
+# sentence contains words that are contained in
+# wordFeatures
 featureSets = []
-
 for (s, c) in documents:
     featureSets.append((findFeatures(s), c))
 
@@ -93,6 +106,10 @@ pickle.dump(featureSets, savedFeatureSets)
 savedFeatureSets.close()
 
 
+# Trains each of the five classifiers up to 50 times
+# after randomly shuffling the feature sets, takes
+# the best accuracy result recorded, and saves
+# classifiers to .pickle files
 maxAvg = 0
 maxMNB = 0
 maxBNB = 0
@@ -100,16 +117,13 @@ maxLReg = 0
 maxSGD = 0
 maxLSVC = 0
 
-
-while True:
+for i in range(50):
     random.shuffle(featureSets)
     print(len(featureSets))
-
 
     # Training and Testing Set
     trainingSet = featureSets[:9740]
     testingSet = featureSets[9740:]
-
 
     # Multinomial Naive Bayes Classifier
     MultinomialNBClassifier = SklearnClassifier(MultinomialNB())
@@ -137,48 +151,45 @@ while True:
     accuracySGD = (nltk.classify.accuracy(StochasticGradientDescentClassifier, testingSet)) * 100
     accuracyLSVC = (nltk.classify.accuracy(LinearSVCClassifier, testingSet)) * 100
 
-    avg = (accuracyMNB + accuracyBNB + accuracyLReg + accuracySGD + accuracyLSVC) / 5
+    average = (accuracyMNB + accuracyBNB + accuracyLReg + accuracySGD + accuracyLSVC) / 5
 
-    if avg > maxAvg:
-        maxAvg = avg
+    if average > maxAvg:
+        maxAvg = average
         maxMNB = accuracyMNB
         maxBNB = accuracyBNB
         maxLReg = accuracyLReg
         maxSGD = accuracySGD
         maxLSVC = accuracyLSVC
 
-        saveClassifier = open('mnb.pickle', 'wb')
+        saveClassifier = open('trainedClassifiers/mnb.pickle', 'wb')
         pickle.dump(MultinomialNBClassifier, saveClassifier)
         saveClassifier.close()
 
-        saveClassifier = open('bnb.pickle', 'wb')
+        saveClassifier = open('trainedClassifiers/bnb.pickle', 'wb')
         pickle.dump(BernoulliNBClassifier, saveClassifier)
         saveClassifier.close()
 
-        saveClassifier = open('lreg.pickle', 'wb')
+        saveClassifier = open('trainedClassifiers/lreg.pickle', 'wb')
         pickle.dump(LogisticRegressionclassifier, saveClassifier)
         saveClassifier.close()
 
-        saveClassifier = open('sgd.pickle', 'wb')
+        saveClassifier = open('trainedClassifiers/sgd.pickle', 'wb')
         pickle.dump(StochasticGradientDescentClassifier, saveClassifier)
         saveClassifier.close()
 
-        saveClassifier = open('lsvc.pickle', 'wb')
+        saveClassifier = open('trainedClassifiers/lsvc.pickle', 'wb')
         pickle.dump(LinearSVCClassifier, saveClassifier)
         saveClassifier.close()
 
-    print('Average: ', avg)
+    print('Average: ', average)
     print('MNB: ', accuracyMNB)
     print('BNB: ', accuracyBNB)
     print('LReg: ', accuracyLReg)
     print('SGD: ', accuracySGD)
-    print('LSVC: ', accuracyLSVC)
-    print()
-
+    print('LSVC: ', accuracyLSVC, '\n')
     print('Max Average: ', maxAvg)
     print('Max MNB: ', maxMNB)
     print('Max BNB: ', maxBNB)
     print('Max LReg: ', maxLReg)
     print('Max SGD: ', maxSGD)
-    print('Max LSVC: ', maxLSVC)
-    print()
+    print('Max LSVC: ', maxLSVC, '\n')
